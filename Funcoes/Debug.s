bb57fa6bd4bc443762e913bcae88ee52813b02ac
João Pedro Barros Silva Azevedo <jp67.azevedo@gmail.com>
2026-05-08 09:55:36 -0300

    Sleep, troca de frames
    Adicionadas as funções sleep, pequenos
    códigos para troca de frame e princípio funcional do renderiza background

--- Funcoes/Debug.s
+++ Funcoes/Debug.s
@@ -1,3 +1,22 @@
-	la a0, texto
-	li a7, 4
+.data
+
+.text
+	# Inverte frame a ser renderizado
+LOOP:	
+	li t0, 0xFF200604
+	lw t1, 0(t0)
+	xori t1, t1, 1
+	slli t1, t1, 20
+	
+	lui t2, 0xFF000
+	xor t1, t1, t2
+	mv s1, t1	#S1 = 0xFFX0 0000 : Endereco inicial do Bitmap a ser renderizado
+		
+	li t0, 0xFF200604
+	lw t1, 0(t0)
+	xori t1, t1, 1
+	sw t1 0(t0)
+	
+	j LOOP
+	li a7,10
 	ecall
