359345cff90793d504012ab612bcb69838d07f88
Mediador09 <fer.amdias@gmail.com>
2026-06-01 13:13:52 -0300

    feat: atualizar proc de adicionar entidade para retornar endereco da entidade adicionada

--- src/entidades/adicionar_entidade.s
+++ src/entidades/adicionar_entidade.s
@@ -6,14 +6,15 @@
 #       a1 - posicao X (absoluta)
 #       a2 - posicao Y (absoluta)
 # RETORNO
-#       (nenhum)
+#       a0 - endereco da entidade registrada
 
 PROC_ADICIONAR_ENTIDADE:
-        addi sp, sp, -16
+        addi sp, sp, -20
         sw ra, (sp)
         sw s0, 4(sp)
         sw s1, 8(sp)
         sw s2, 12(sp)
+        sw s3, 16(sp)
 
         la s0, lista_entidades
         mv s1, a1       # salva X
@@ -21,7 +22,7 @@ PROC_ADICIONAR_ENTIDADE:
 
 P_AE1_PROCURAR_LOOP:
         lw t1, lista_entidades.TIPO_ENTRADA(s0) # pega o tipo da entrada na tabela
-        bltz t1, P_AE1_RET              # se < 0, significa que chegamos ao fim da tabela sem encontrara a entidade.
+        bltz t1, P_AE1_RET_NULL         # se < 0, significa que chegamos ao fim da tabela sem encontrara a entidade.
         beq a0, t1, P_AE1_REGISTRAR     # se tipo_tabela == tipo_argumento, encontramos! registrar
         addi s0, s0, lista_entidades.BYTES_POR_ENTRADA # pula para a proxima entrada
         j P_AE1_PROCURAR_LOOP           # continua procurando se nao encontramos ainda
@@ -31,7 +32,10 @@ P_AE1_REGISTRAR:
         lw t0, lista_entidades.TAMANHO_STRUCT_ENTRADA(s0)
         li t1, struct_basica_entidade.TAMANHO
         add a0, t0, t1 
-        jal PROC_MALLOC         # aloca espaco para a struct basica e a struct especifica!   
+        jal PROC_MALLOC         # aloca espaco para a struct basica e a struct especifica!  
+        beqz a0, P_AE1_RET_NULL # retorna nulo se acabou o espaco
+
+        mv s3, a0                       # guarda o endereco da entidade 
 
         # pula pro proximo espaco livre no array de *entidades*
         la t3, array_entidades
@@ -44,7 +48,7 @@ P_AE1_REGISTRAR:
         sw t5, (t4)
 
         # salva a entidade
-        sw a0, array_entidades.STRUCT_BASICA(t3)
+        sw s3, array_entidades.STRUCT_BASICA(t3)
         lw t5, lista_entidades.PROC_POR_FRAME(s0)
         sw t5, array_entidades.PROC_POR_FRAME(t3)
         lw t5, lista_entidades.PROC_DESENHAR(s0)
@@ -52,12 +56,12 @@ P_AE1_REGISTRAR:
 
         # guarda a referencia para a struct especifica dela
         # vamos guarda-la imediatamente depois da struct basica, no espaco alocado!
-        addi t0, a0, struct_basica_entidade.TAMANHO 
-        sw t0, entidade.STRUCT_ESPECIFICA(a0)
+        addi t0, s3, struct_basica_entidade.TAMANHO 
+        sw t0, entidade.STRUCT_ESPECIFICA(s3)
 
         # guarda posicao tbm
-        sw s1, entidade.X(a0)
-        sw s2, entidade.Y(a0)
+        sw s1, entidade.X(s3)
+        sw s2, entidade.Y(s3)
 
         # chama a funcao de criacao passando a struct basica e a posicao X e Y
         # a0 - STRUCT BASICA - jah posicionado
@@ -65,11 +69,18 @@ P_AE1_REGISTRAR:
         mv a2, s2               # recupera Y
         lw t0, lista_entidades.PROC_DE_CRIACAO(s0)
         jalr ra, t0, 0          # pula pro procedimento de criacao da entidade
+
+        mv a0, s3               # retorna a entidade
         
 P_AE1_RET:
         lw ra, (sp)
         lw s0, 4(sp)
         lw s1, 8(sp)
         lw s2, 12(sp)
-        addi sp, sp, 16
+        lw s3, 16(sp)
+        addi sp, sp, 20
         ret
+
+P_AE1_RET_NULL:                 # retorna nulo
+        mv a0, zero
+        j P_AE1_RET
