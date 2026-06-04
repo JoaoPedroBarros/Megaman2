9d611a5210d8eb9dac7356f8779c6862fa89b799
Mediador09 <fer.amdias@gmail.com>
2026-06-03 22:58:59 -0300

    docs: corrigir comentários

--- src/aritmetica/max.s
+++ src/aritmetica/max.s
@@ -9,7 +9,7 @@
 # a0 - maximo
 
 PROC_MAX:
-        bge a0, a1, PM2_RET     # retorna o proprio a0 se a0 <= a1
-        mv a0, a1               # retorna a1 se a1 > a0
+        bge a0, a1, PM2_RET     # retorna o proprio a0 se a0 >= a1
+        mv a0, a1               # retorna a1 se a0 < a1
 PM2_RET:
 ret
--- src/aritmetica/min.s
+++ src/aritmetica/min.s
@@ -10,6 +10,6 @@
 
 PROC_MIN:
         blt a0, a1, PM1_RET     # retorna o proprio a0 se a0 < a1
-        mv a0, a1               # retorna a1 se a1 <= a0
+        mv a0, a1               # retorna a1 se a1 >= a0
 PM1_RET:
 ret
