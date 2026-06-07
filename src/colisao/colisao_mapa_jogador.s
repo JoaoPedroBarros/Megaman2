4935db6a5aae0f379d6b6bc8c7ad3c4fbc09784d
vitor2828 <ak.jvitor28@gmail.com>
2026-06-07 16:59:19 -0300

    feat: colisao horizontal

---
+++ src/colisao/colisao_mapa_jogador.s
@@ -0,0 +1,96 @@
+### PROCEDIMENTOS PARA ANALISAR COLISAO COM O MAPA ###
+
+### ARGUMENTOS ###
+
+# a0 - struct basica do jogador
+
+### RETORNO ###
+
+# a0 - 0 se o jogador nao pode se movimentar. 1 caso contrario.
+
+
+PROC_COLISAO_MAPA_DIREITA:
+
+        la t0, tilemap # carrega a referencia do tilemap
+
+        lw t1, entidade.X_Q12(a0) # carrega o X
+        lw t2, entidade.Y_Q12(a0) # carrega o Y
+
+        srli t1, t1, 12 # volta as coordenadas para Q0
+        srli t2, t2, 12
+
+        addi t1, t1, 28 # soma 24 para o fim do sprite: fica melhor visualmente
+
+        li t3, TAMANHO_TILE # carrega o tamanho do tile
+
+        div t1, t1, t3 # divide as coordenadas para obter a localizacao no tilemap
+        div t2, t2, t3
+
+        lw t3, 4(t0) # calcula o comprimento do tilemap
+
+        mul t2, t2, t3 # multiplica o Y pelo comprimento para saber quantos tiles ja percorreu verticalmente
+        add t1, t1, t2 # adiciona o X para saber quantos tiles ja percorreu horizontalmente
+
+        addi t0, t0, 8 # pula as dimensoes do mapa
+        add t0, t0, t1 # adiciona a localizacao do personagem ao primeiro tile
+
+        lb t0, 0(t0) # carrega o tile
+        safe_print_int_ln(t0)
+        beq t0, zero, MOVIMENTACAO_INVALIDA # 0 eh o tile preto: nao move
+        li t1, 2
+        beq t0, t1, MOVIMENTACAO_INVALIDA # 2 eh um tile de chao: nao move
+        li t1, 3
+        beq t0, t1, MOVIMENTACAO_INVALIDA # 3 eh um tile de chao: nao move
+        j MOVIMENTACAO_VALIDA
+
+
+PROC_COLISAO_MAPA_ESQUERDA:
+
+        la t0, tilemap # carrega a referencia do tilemap
+
+        lw t1, entidade.X_Q12(a0) # carrega o X
+        lw t2, entidade.Y_Q12(a0) # carrega o Y
+
+        srli t1, t1, 12 # volta as coordenadas para Q0
+        srli t2, t2, 12
+
+        ## como a esquerda ja pega o inicio do sprite, nao precisa de aritmetica pra deixar melhor visualmente
+
+        li t3, TAMANHO_TILE # carrega o tamanho do tile
+
+        div t1, t1, t3 # divide as coordenadas para obter a localizacao no tilemap
+        div t2, t2, t3
+
+        lw t3, 4(t0) # calcula o comprimento do tilemap
+
+        mul t2, t2, t3 # multiplica o Y pelo comprimento para saber quantos tiles ja percorreu verticalmente
+        add t1, t1, t2 # adiciona o X para saber quantos tiles ja percorreu horizontalmente
+
+        addi t0, t0, 8 # pula as dimensoes do mapa
+        add t0, t0, t1 # adiciona a localizacao do personagem ao primeiro tile
+
+        lb t0, 0(t0) # carrega o tile
+        safe_print_int_ln(t0)
+        beq t0, zero, MOVIMENTACAO_INVALIDA # 0 eh o tile preto: nao move
+        li t1, 2
+        beq t0, t1, MOVIMENTACAO_INVALIDA # 2 eh um tile de chao: nao move
+        li t1, 3
+        beq t0, t1, MOVIMENTACAO_INVALIDA # 3 eh um tile de chao: nao move
+        li t1, 9
+        beq t0, t1, MOVIMENTACAO_INVALIDA
+        j MOVIMENTACAO_VALIDA
+
+PROC_COLISAO_MAPA_BAIXO:
+
+PROC_COLISAO_MAPA_CIMA:
+
+MOVIMENTACAO_INVALIDA:
+
+        li a0, 0 # 0 nao deixa o personagem completar o movimento
+        ret
+
+MOVIMENTACAO_VALIDA:
+
+        li a0, 1 # 1 deixa o personagem completar o movimento
+        ret
+
--- src/entradas/processar_entradas.s
+++ src/entradas/processar_entradas.s
@@ -57,15 +57,29 @@ P_PE1_W:
         j P_PE1_RET
 
 P_PE1_A:
-        lw t1, entidade.X_Q12(a0)
-        li t0, -4
-        slli t0, t0, 12
-        add t1, t0, t1
-        sw t1, entidade.X_Q12(a0)
+        addi sp, sp, -8
+        sw a0, 0(sp) # guarda a referencia pra entidade basica
+        sw ra, 4(sp) # return adress
 
-        lw t1, entidade.STRUCT_ESPECIFICA(a0)
+        jal PROC_COLISAO_MAPA_ESQUERDA 
+        mv t0, a0 # move o resultado do procedimento pra t0
+        lw a0, 0(sp) # restaura a entidade basica
+        lw ra, 4(sp) # restaura o return adress
+        addi sp, sp, 8
+
+        beq t0, zero, SEM_MOVIMENTO_ESQUERDA # se t0 for 0, identificou uma colisao: nao move
+
+        lw t1, entidade.X_Q12(a0) # caso contrario, pega o endereco do t1 e adiciona, em Q12, o movimento
+        li t0, -4 # quantidade do movimento
+        slli t0, t0, 12 # colocando o t0 em Q12
+        add t1, t0, t1 # adicionando o movimento
+        sw t1, entidade.X_Q12(a0) # salvando o movimento na struct
+
+SEM_MOVIMENTO_ESQUERDA:
+
         li t0, -1
-        sb t0, JOGADOR.DIRECAO(t1)      # salva para tras
+        lw t1, entidade.STRUCT_ESPECIFICA(a0)
+        sb t0, JOGADOR.DIRECAO(t1)      # salva para frente
 
         j P_PE1_RET
 
@@ -74,11 +88,25 @@ P_PE1_S:
         j P_PE1_RET
 
 P_PE1_D:
-        lw t1, entidade.X_Q12(a0)
-        li t0, 4
-        slli t0, t0, 12
-        add t1, t0, t1
-        sw t1, entidade.X_Q12(a0)
+        addi sp, sp, -8
+        sw a0, 0(sp) # guarda a referencia pra entidade basica
+        sw ra, 4(sp) # return adress
+
+        jal PROC_COLISAO_MAPA_DIREITA 
+        mv t0, a0 # move o resultado do procedimento pra t0
+        lw a0, 0(sp) # restaura a entidade basica
+        lw ra, 4(sp) # restaura o return adress
+        addi sp, sp, 8
+
+        beq t0, zero, SEM_MOVIMENTO_DIREITA # se t0 for 0, identificou uma colisao: nao move
+
+        lw t1, entidade.X_Q12(a0) # caso contrario, pega o endereco do t1 e adiciona, em Q12, o movimento
+        li t0, 4 # quantidade do movimento
+        slli t0, t0, 12 # colocando o t0 em Q12
+        add t1, t0, t1 # adicionando o movimento
+        sw t1, entidade.X_Q12(a0) # salvando o movimento na struct
+
+SEM_MOVIMENTO_DIREITA:
 
         li t0, 1
         lw t1, entidade.STRUCT_ESPECIFICA(a0)
