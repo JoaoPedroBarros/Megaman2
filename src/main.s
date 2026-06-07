30f4a798bca1a9f4db5170c849af78a7d8769030
Mediador09 <fer.amdias@gmail.com>
2026-06-06 23:34:20 -0300

    feat: adicionar proc de imprimir retangulo

--- src/main.s
+++ src/main.s
@@ -38,6 +38,7 @@ main:
 .include "impressao_e_tela/imprimir_textura.s"
 .include "impressao_e_tela/imprimir_tile.s"
 .include "impressao_e_tela/imprimir_entidades.s"
+.include "impressao_e_tela/imprimir_retangulo.s"
 .include "entidades/adicionar_entidade.s"
 .include "entidades/entidades_manager.s"
 .include "entidades/remover_entidade.s"
