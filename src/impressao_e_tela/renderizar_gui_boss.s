75c49ad521227c314700846ad0429674b5116b9d
Mediador09 <fer.amdias@gmail.com>
2026-07-09 19:43:07 -0300

    tweak: corrigir comentarios e remover print de debug

--- src/impressao_e_tela/renderizar_gui_boss.s
+++ src/impressao_e_tela/renderizar_gui_boss.s
@@ -19,15 +19,13 @@ P_RG1_IMPRIMIR_BARRA_DE_VIDA_BOSS:
 
     li t2, 100
     mul t1, t1, t2
-    div t1, t1, t0  # pega a porcentagem de vida do jogador
+    div t1, t1, t0  # pega a porcentagem de vida do boss
 
-    safe_print_int_ln(t1)
-
-    li a0, 0x0F           # barra vermelha
-    li a1, 296          # x1 = 16
-    li a3, 300           # x2 = 20
-    li a4, 190            # y2 = 200
-    sub a2, a4, t1        # y1 = 200 - % 
+    li a0, 0x0F             # barra vermelha
+    li a1, 296              # x1 = 296
+    li a3, 300              # x2 = 300
+    li a4, 190              # y2 = 190
+    sub a2, a4, t1          # y1 = 190 - % 
     jal PROC_IMPRIMIR_RETANGULO
 
     li a0, 0xFF
