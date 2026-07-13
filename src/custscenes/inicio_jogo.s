cf07621369d0a1f3a33364bf1b5d8e51712cc3be
vitor2828 <ak.jvitor28@gmail.com>
2026-07-13 18:34:22 -0300

    tweak: adições à tela inicial

--- src/custscenes/inicio_jogo.s
+++ src/custscenes/inicio_jogo.s
@@ -4,6 +4,8 @@
 .data
 
     TITULO_JOGO: .asciz "FLORA, THE WITCH"
+    INICIAR_JOGO: .asciz "1 - Iniciar jogo"
+    SAIR_JOGO:  .asciz "0 - Sair do Jogo"
 
 .text
 
@@ -31,7 +33,7 @@ P_MENU_LOOP:
         lw a1, FRAME_ATUAL
         seqz a1, a1
         li a7, 148
-        ecall
+        #ecall
 
         jal PROC_DIALOGOS_MANAGER
         
@@ -44,6 +46,22 @@ P_MENU_LOOP:
         li a1, 103
         li a2, 120
         li a3, 0x14
+        lw a4, FRAME_ATUAL
+        li a7, 104
+        ecall
+
+        la a0, INICIAR_JOGO
+        li a1, 103
+        li a2, 160
+        li a3, 0xFF
+        lw a4, FRAME_ATUAL
+        li a7, 104
+        ecall
+
+        la a0, SAIR_JOGO
+        li a1, 103
+        li a2, 180
+        li a3, 0xFF
         lw a4, FRAME_ATUAL
         li a7, 104
         ecall
