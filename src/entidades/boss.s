a2a3bc99355a95d8cc17aa9c8c2ea02dfe6c8379
Mediador09 <fer.amdias@gmail.com>
2026-07-15 00:49:37 -0500

    feat: criar flags de retorno para a funcao de fase e agendamento de retorno

--- src/entidades/boss.s
+++ src/entidades/boss.s
@@ -34,13 +34,13 @@ BOSS.NOVO:
     sw t0, entidade.COLIDIVEL(a0)
     sw t0, entidade.HOSTIL(a0)
 
-    definir_hitbox(a0, 5, 6, 22, 26)    # temporario! vai depender no sprite final
+    definir_hitbox(a0, 2, 14, 34, 36)    # temporario! vai depender no sprite final
 
     # nao havera tratamento de gravidade para o chefe, visto que ele voarah
 
     lw t0, entidade.STRUCT_ESPECIFICA(a0)
 
-    li t1, 1000 # o chefe tera 1000 de vida
+    li t1, 100 # o chefe tera 1000 de vida
     sw t1, BOSS.VIDA(t0)
     sw t1, BOSS.VIDA_MAXIMA(t0)
 
@@ -340,8 +340,11 @@ BOSS.COLISAO._PROJETIL_COMUM:
     bnez a0, BOSS.COLISAO._RET
 
     lw t1, BOSS.REFERENCIA_CLONE(t0)
-    lw t1, entidade.STRUCT_ESPECIFICA(t1)
-    sw zero, BOSS.VIDA(t1)
+
+    # mata o outro tbm
+    lw t2, entidade.FLAGS(t1)
+    ori t2, t2, FLAG_ENTIDADE_DELECAO_PENDENTE
+    sw t2, entidade.FLAGS(t1)
 
     j BOSS.COLISAO._RET
 
@@ -388,6 +391,25 @@ SEM_SEGUNDA_FASE:
     lw ra, 0(sp)
     lw s0, 4(sp)
     addi sp, sp, 8
+    ret
+
+BOSS.DESTRUTOR:
+    addi sp, sp, -4
+    sw ra, (sp)
+    
+    lw t0, entidade.STRUCT_ESPECIFICA(a0)
+    lb t1, BOSS.CLONE(t0)
+    bgtz t1, BOSS.DESTRUTOR._RET
+
+    # se o boss nao era clone, agenda a vitoria do jogador
+
+    li a0, FLAG_RETORNO_VITORIA
+    li a1, 15
+    jal PROC_AGENDAR_RETORNO_FASE
+
+BOSS.DESTRUTOR._RET:
+    lw ra, (sp)
+    addi sp, sp, 4
     ret
 
 
---
+++ src/fase_e_mapa/agendar_retorno_fase.s
@@ -0,0 +1,29 @@
+# PROC_AGENDAR_RETORNO_FASE
+#
+# Prepara para sair da fase com um valor de retorno especifico.
+#
+# Argumento:
+# a0 - valor de retorno
+# a1 - quantos frames no futuro (0: imediatamente)
+#
+# Sem retornos.
+
+.text
+PROC_AGENDAR_RETORNO_FASE:
+        addi sp, sp, -4
+        sw ra, (sp)
+
+        lw t0, frame_counter
+        add a2, a1, t0          # (a1 original) frames no futuro
+        mv a1, a0
+        la a0, SUBPROC_FINALIZAR
+        jal PROC_ADICIONAR_EVENTO       # agenda a execucao da subproc para retornar com o valor desejado
+
+        lw ra, (sp)
+        addi sp, sp, 4
+        ret
+
+# arg: a0 - valor
+SUBPROC_FINALIZAR:
+        sw a0, flagretornofase, t0
+        ret
