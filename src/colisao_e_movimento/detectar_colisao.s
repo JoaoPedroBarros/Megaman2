a632efc24bbdb80320cdf9b2de75139f5772388a
Mediador09 <fer.amdias@gmail.com>
2026-07-16 11:35:39 -0500

    fix: corrigir algoritmo de colisao

--- src/colisao_e_movimento/detectar_colisao.s
+++ src/colisao_e_movimento/detectar_colisao.s
@@ -16,10 +16,19 @@
 
 
 # algoritmo AABB:
-# retorna a.x <= b.x + b.largura &&
-#         a.x + a.largura >= b.x &&
-#         a.y <= b.y + b.altura &&
-#         a.y + a.altura >= b.y.
+#. x1 = a.x + a.offx
+#. x2 = b.x + b.offx
+#.
+#. if (x1 > x2 + b.width) return 0;
+#. if (x2 > x1 + a.width) return 0;
+#.
+#. y1 = a.y + a.offy
+#. y2 = b.y + b.offy
+#.
+#. if (y1 > y2 + b.height) return 0;
+#. if (y2 > y1 + a.height) return 0;
+#.
+#. return 1;
 
 PROC_DETECTAR_COLISAO:
         lw t0, entidade.X_Q12(a0)
@@ -29,11 +38,11 @@ PROC_DETECTAR_COLISAO:
 
         lw t1, entidade.X_Q12(a1)
         srai t1, t1, 12 
-        lw t2, entidade.HITBOX_DESLOCAMENTO_X(a0)
-        add t0, t0, t2 # entidade2.x
+        lw t2, entidade.HITBOX_DESLOCAMENTO_X(a1)
+        add t1, t1, t2 # entidade2.x
 
         lw t2, entidade.HITBOX_LARGURA(a0)
-        lw t3, entidade.HITBOX_ALTURA(a1)
+        lw t3, entidade.HITBOX_LARGURA(a1)
 
         add t4, t1, t3
         bgt t0, t4, P_DC1_SEM_COLISAO
@@ -47,11 +56,11 @@ PROC_DETECTAR_COLISAO:
 
         lw t1, entidade.Y_Q12(a1)
         srai t1, t1, 12
-        lw t2, entidade.HITBOX_DESLOCAMENTO_X(a0)
-        add t0, t0, t2 # entidade2.Y
+        lw t2, entidade.HITBOX_DESLOCAMENTO_Y(a1)
+        add t1, t1, t2 # entidade2.Y
 
-        lw t2, entidade.HITBOX_LARGURA(a0)
-        lw t3, entidade.HITBOX_LARGURA(a1)
+        lw t2, entidade.HITBOX_ALTURA(a0)
+        lw t3, entidade.HITBOX_ALTURA(a1)
 
         add t4, t1, t3
         bgt t0, t4, P_DC1_SEM_COLISAO
